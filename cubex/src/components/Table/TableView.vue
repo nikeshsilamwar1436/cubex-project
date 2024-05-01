3453d0693e18159df88c9e35e5bda9eecd4d7d6c
nikeshsilamwar1436 <nikeshkumars001@gmail.com>
2024-05-01 14:25:48 +0100

    .net project

---
+++ cubex/src/components/Table/TableView.vue
@@ -0,0 +1,33 @@
+﻿<template>
+  <div class="q-pa-md">
+    <q-table
+      class="my-sticky-header-column-table"
+      :title="title"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      :pagination="pagination"
+    />
+  </div>
+</template>
+<script lang="ts" setup>
+import {tableColumns} from 'components/models';
+import {ref} from 'vue';
+
+defineProps({
+  rows: {
+    type: Array,
+    default: null
+  },
+  columns: {
+    type: Array<tableColumns>,
+  },
+  title: String
+});
+const pagination = ref({
+  sortBy: 'desc',
+  descending: false,
+  page: 1,
+  rowsPerPage: 10
+})
+</script>
